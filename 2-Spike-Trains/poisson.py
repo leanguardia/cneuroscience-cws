9ff036ef5c651f05796421a7b25bf314369c6e5b
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-24 20:17:28 +0100

    Input paremeters in Poisson distribution

--- 2-Spike-Trains/poisson.py
+++ 2-Spike-Trains/poisson.py
@@ -1,37 +1,45 @@
 import random as rnd
 
-def get_spike_train(rate,big_t,tau_ref):
+def get_spike_train(rate, big_t, tau_ref):
 
-    if 1<=rate*tau_ref:
+    if 1 <= rate * tau_ref:
         print("firing rate not possible given refractory period f/p")
         return []
 
+    exp_rate = rate / (1 - tau_ref * rate)
+    spike_train = []
 
-    exp_rate=rate/(1-tau_ref*rate)
-
-    spike_train=[]
-
-    t=rnd.expovariate(exp_rate)
-
-    while t< big_t:
+    t = rnd.expovariate(exp_rate)
+    while t < big_t:
         spike_train.append(t)
-        t+=tau_ref+rnd.expovariate(exp_rate)
+        t += tau_ref + rnd.expovariate(exp_rate)
 
     return spike_train
 
+######### MAIN #########
 
+Hz = 1.0
+sec = 1.0
+ms = 0.001
 
-Hz=1.0
-sec=1.0
-ms=0.001
+rate = 15.0 * Hz
+tau_ref = 5 * ms
 
-rate=15.0 *Hz
-tau_ref=5*ms
+big_t = 5 * sec
 
-big_t=5*sec
+print("Input params")
+print("Hz:", Hz)
+print("sec:", sec)
+print("ms:", ms)
+print("rate:", rate)
+print("tau_ref:", tau_ref)
+print("big_t:", big_t)
 
-spike_train=get_spike_train(rate,big_t,tau_ref)
+spike_train = get_spike_train(rate, big_t, tau_ref)
 
-print(len(spike_train)/big_t)
+length = len(spike_train)
+print("Length", length)
 
-print(spike_train)
+print("XYZ", length / big_t)
+
+# print(spike_train)
