a0f269cd994dc1c71c09705a4e722f96e0b509e4
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-24 21:25:11 +0100

    Q1 Fano Factor Spike Counts

--- 2-Spike-Trains/poisson.py
+++ 2-Spike-Trains/poisson.py
@@ -20,6 +20,17 @@ def get_spike_train(rate, big_t, tau_ref):
 
     return spike_train
 
+def spike_counts(spike_train, big_t, interval_window):
+    total_bins = int(big_t / interval_window)
+    spike_counts = np.zeros(total_bins)
+    bin = 1
+    for spike_time in spike_train:
+        if spike_time > (bin * interval_window): bin += 1
+        spike_counts[bin - 1] += 1
+    return spike_counts
+
+# def fano_factor(spike_train, big_t, interval_window):
+
 ######### MAIN #########
 
 Hz = 1.0
@@ -40,5 +51,11 @@ spikes_count = len(spike_train)
 
 print("Spikes", spikes_count)
 print("-->", spikes_count / big_t, "spikes/sec")
+print(spike_train)
 
-# print(spike_train)
+# Q1
+# Fano Factor
+interval_window = 100 * ms
+spike_counts = spike_counts(spike_train, big_t, interval_window)
+print(len(spike_counts))
+print(spike_counts)
