1311a766e750c0b9812789eb75fe3e02effb97cb
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-25 01:32:25 +0100

    Q2 Coeff of Var

--- 2-Spike-Trains/load.py
+++ 2-Spike-Trains/load.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from poisson import coefficient_of_variation
 
 def load_data(filename, Type):
     data_array = [Type(line.strip()) for line in open(filename, 'r')]
@@ -16,7 +17,8 @@ def generate_spiking_times(event_sequence, sample_time = 0.002):
 file_path = os.getenv('ABS_PATH') + "/rho.dat"
 spikes = load_data(file_path, int)
 
-print(len(spikes))
+print("Lenght of data:",len(spikes))
+print("Firing Events:", np.sum(spikes))
 print(spikes[0:18])
 
 ##### Q2 #####
@@ -26,13 +28,16 @@ spikes_mean = np.mean(spikes)
 spikes_var = np.var(spikes)
 
 fano_factor = spikes_var / spikes_mean
-print("Fano Factor", fano_factor)
+print("Fano Factor:", fano_factor)
 
 # Coeff of Variance
 spike_train = generate_spiking_times(spikes)
-print(len(spike_train)) # is equal to np.sum(spikes)
-print(spike_train[0:10])
-print(spike_train[-10:])
+print("Activations detected:", len(spike_train))
+# print(spike_train[0:10])
+# print(spike_train[-10:])
+
+coeff_of_var = coefficient_of_variation(spike_train)
+print("Coeff of Var:", coeff_of_var)
 
 #stimulus=[float(x) for x in load_data("stim.dat")]
 # stimulus = load_data("stim.dat",float)
