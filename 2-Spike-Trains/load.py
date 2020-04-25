fe5c7d3668b6f7499e1f3687955cbd1337373a5f
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-25 01:20:41 +0100

    Q2 Fano Factor

--- 2-Spike-Trains/load.py
+++ 2-Spike-Trains/load.py
@@ -5,18 +5,34 @@ def load_data(filename, Type):
     data_array = [Type(line.strip()) for line in open(filename, 'r')]
     return data_array
 
+def generate_spiking_times(event_sequence, sample_time = 0.002):
+  elapsed_time = 0
+  spike_train = []
+  for event in event_sequence:
+    if event: spike_train.append(elapsed_time)
+    elapsed_time += sample_time
+  return spike_train
+
 file_path = os.getenv('ABS_PATH') + "/rho.dat"
 spikes = load_data(file_path, int)
 
 print(len(spikes))
 print(spikes[0:18])
 
+##### Q2 #####
+
+# Fano Factor
 spikes_mean = np.mean(spikes)
 spikes_var = np.var(spikes)
 
 fano_factor = spikes_var / spikes_mean
-
 print("Fano Factor", fano_factor)
+
+# Coeff of Variance
+spike_train = generate_spiking_times(spikes)
+print(len(spike_train)) # is equal to np.sum(spikes)
+print(spike_train[0:10])
+print(spike_train[-10:])
 
 #stimulus=[float(x) for x in load_data("stim.dat")]
 # stimulus = load_data("stim.dat",float)
