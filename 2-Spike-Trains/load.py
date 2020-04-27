5cebb67ad337d17a557a3cea3abdc326ec1892b6
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-27 12:38:21 +0100

    Q2 Fix

--- 2-Spike-Trains/load.py
+++ 2-Spike-Trains/load.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from poisson import coefficient_of_variation
+from poisson import fano_factor, coefficient_of_variation
 
 def load_data(filename, Type):
     data_array = [Type(line.strip()) for line in open(filename, 'r')]
@@ -17,26 +17,28 @@ def generate_spiking_times(event_sequence, sample_time = 0.002):
 
 ##### Q2 #####
 if __name__ == "__main__":
+  
   file_path = os.getenv('ABS_PATH') + "/rho.dat"
   spikes = load_data(file_path, int)
 
   print("Lenght of data:",len(spikes))
   print("Firing Events:", np.sum(spikes))
-  print(spikes[0:18])
+  # print(spikes[0:18])
 
-  # Fano Factor
-  spikes_mean = np.mean(spikes)
-  spikes_var = np.var(spikes)
-
-  fano_factor = spikes_var / spikes_mean
-  print("Fano Factor:", fano_factor)
-
-  # Coeff of Variance
+  # Generate Spike Train
   spike_train = generate_spiking_times(spikes)
   print("Activations detected:", len(spike_train))
   # print(spike_train[0:10])
   # print(spike_train[-10:])
+  # print(spike_train[:-10])
 
+  # Fano Factor
+  big_t = 1200 # seconds  
+  interval_window = 10
+  fano_factor = fano_factor(spike_train, big_t, interval_window)
+  print("Fano Factor:", fano_factor)
+
+  # Coeff of Variance
   coeff_of_var = coefficient_of_variation(spike_train)
   print("Coeff of Var:", coeff_of_var)
 
